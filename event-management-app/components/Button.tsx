cbfec95964a4e56b1077f2d1ac8b885139abca29
Amitav Panda <pandaamitav01@gmail.com>
2023-12-09 19:49:51 +0530

    add hero section

--- event-management-app/components/Button.tsx
+++ event-management-app/components/Button.tsx
@@ -5,10 +5,11 @@ type ButtonProps = {
     title : string;
     icon? : string;
     variant : 'btn_dark_black';
+    hoverBgVariant : 'btn_white_text';
 }
-function Button({ type, title, icon, variant} : ButtonProps) {
+function Button({ type, title, icon, variant, hoverBgVariant} : ButtonProps) {
   return (
-    <button type={type}>
+    <button type={type} className={`flexCenter gap-3 border ${variant} w-50 h-10 mt-5 lg:mt-10 ${hoverBgVariant} cursor-pointer`}>
         {icon && <Image src={icon} alt={title} width={24} height={24}/>}
         <label>{title}</label>
     </button>
