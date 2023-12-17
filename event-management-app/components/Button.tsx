7360afece3f5b1bca624a75754c23009f17523a7
Amitav Panda <pandaamitav01@gmail.com>
2023-12-17 19:40:53 +0530

    add Services Section

--- event-management-app/components/Button.tsx
+++ event-management-app/components/Button.tsx
@@ -6,14 +6,28 @@ type ButtonProps = {
     icon? : string;
     variant : 'btn_dark_black';
     hoverBgVariant : 'btn_white_text';
+    height?: number;
+    width?: number;
 }
-function Button({ type, title, icon, variant, hoverBgVariant} : ButtonProps) {
+
+const Button: React.FC<ButtonProps> = ({ type, title, icon, variant, hoverBgVariant, height, width }) => {
+  const buttonStyle: React.CSSProperties = {
+    height: height ? `${height}rem` : undefined,
+    width: width ? `${width}rem` : undefined,
+  };
+
   return (
-    <button type={type} className={`flexCenter gap-3 border ${variant} w-50 h-10 mt-5 lg:mt-10 ${hoverBgVariant} cursor-pointer`}>
+    <button
+      type={type}
+      className={`flexCenter gap-3 border ${variant} w-50 h-10 mt-5 lg:mt-10 ${hoverBgVariant} cursor-pointer`}
+      style={buttonStyle}
+    >
         {icon && <Image src={icon} alt={title} width={24} height={24}/>}
         <label>{title}</label>
     </button>
-  )
-}
+  );
+};
+
+
 
 export default Button
--- event-management-app/constants/index.ts
+++ event-management-app/constants/index.ts
@@ -1,7 +1,48 @@
+
+
+
 export const NAV_LINKS = [
-    { href: "/", key: "home", label: "Home" },
-    { href: "/", key: "about", label: "About" },
-    { href: "/", key: "services", label: "Services" },
-    { href: "/", key: "contact", label: "Contact" },
+  { href: "/", key: "home", label: "Home" },
+  { href: "/", key: "about", label: "About" },
+  { href: "/", key: "services", label: "Services" },
+  { href: "/", key: "contact", label: "Contact" },
+
+];
+
+
+export const SERVICES = [
+  {
+    title: "Wedding",
+    images: [
+      { src: "/weddingPic.png", alt: "" },
+      { src: "/weddingPic1.png", alt: "" },
+      { src: "/weddingPic2.png", alt: "" },
+      { src: "/weddingPic3.png", alt: "" },
+      { src: "/weddingPic4.png", alt: "" },
+    ],
+    videos: [
+      { src: "weddingVideo1.mp4", type: "video/mp4" },
+      { src: "weddingVideo2.mp4", type: "video/mp4" },
+      { src: "weddingVideo3.mp4", type: "video/mp4" },
+
+    ],
+    buttonLabel: "Details",
+    href: "/service1",
+    key: "wedding",
+  },
+  {
+    title: "Birthday",
+    images: [{ src: "/birthdayPic.png", alt: "" },
+    { src: "/birthdayPic2.png", alt: "" },
+    { src: "/birthdayPic3.png", alt: "" },
+    ],
+    videos: [
+      { src: "birthdayVideo.mp4", type: "birthdayVideo1.mp4" },
+
+    ],
+    buttonLabel: "Details",
+    href: "/service2",
+    key: "birthday",
+  },
 
 ];
