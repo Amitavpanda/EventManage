90adb78755a2b4b5f089c0ff11c0c74ed47e1edc
Amitav Panda <pandaamitav01@gmail.com>
2024-01-11 18:22:40 +0530

    add footer section to the web app

--- event-management-app/components/Button.tsx
+++ event-management-app/components/Button.tsx
@@ -1,30 +1,37 @@
 import Image from "next/image";
+import Link from "next/link";
 
 type ButtonProps = {
-    type : 'button' | 'submit';
-    title : string;
-    icon? : string;
-    variant : 'btn_dark_black';
-    hoverBgVariant : 'btn_white_text';
-    height?: number;
-    width?: number;
+  type: 'button' | 'submit';
+  title: string;
+  icon?: string;
+  variant: string;
+  hoverBgVariant: string;
+  height?: number;
+  width?: number;
+  eventId?: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ type, title, icon, variant, hoverBgVariant, height, width }) => {
+const Button: React.FC<ButtonProps> = ({ type, title, icon, variant, hoverBgVariant, height, width, eventId }) => {
   const buttonStyle: React.CSSProperties = {
     height: height ? `${height}rem` : undefined,
     width: width ? `${width}rem` : undefined,
   };
 
   return (
-    <button
-      type={type}
-      className={`flexCenter gap-3 border ${variant} w-50 h-10 mt-5 lg:mt-10 ${hoverBgVariant} cursor-pointer`}
-      style={buttonStyle}
+    <Link
+      href={`/event-details/${eventId}`} passHref
     >
-        {icon && <Image src={icon} alt={title} width={24} height={24}/>}
+      <button
+        type={type}
+        className={`flexCenter gap-3 border ${variant} w-50 h-10  ${hoverBgVariant} cursor-pointer`}
+        style={buttonStyle}
+      >
+        {icon && <Image src={icon} alt={title} width={24} height={24} />}
         <label>{title}</label>
-    </button>
+      </button>
+    </Link>
+
   );
 };
 
