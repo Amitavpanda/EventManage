e2c2b34ccc5da9d5bb40a667298ae387e0f7cf21
Amitav Panda <pandaamitav01@gmail.com>
2024-04-13 06:43:45 -0700

    changes in the Input  Component to fix deployment errors

--- event-management-app/components/ui/input.tsx
+++ event-management-app/components/ui/input.tsx
@@ -19,14 +19,17 @@ const Input = ({ label, name, placeholder, description, help }: InputProps) => {
     console.log(help)
     return (
         <div className="flex flex-col mt-2">
-            <label className={`${ errors[name]?.message ? 'text-red-500' : 'text-white-100' } text-[0.90rem]`}>{label}</label>
+            <label className={`${errors[name]?.message ? 'text-red-500' : 'text-white-100'} text-[0.90rem]`}>{label}</label>
             <input {...register(name)}
                 className="w-full px-3 overflow-y-hidden py-2 h-10 rounded-md border bg-transparent border-brown-50 focus:outline-none placeholder-brown-30"
                 placeholder={placeholder}
             />
             <span className="text-gray-500">{description}</span>
             <div className={`${errors[name]?.message ? 'h-5' : 'h-3'} mt-1`}>
-                <p className={`text-red-500 text-[0.75rem] ${errors[name] ? '' : 'hidden'}`}> {typeof errors[name]?.message === 'string' ? errors[name]?.message : null}</p>
+                <p className={`text-red-500 text-[0.75rem] ${errors[name].message ? '' : 'hidden'}`}>                 
+                {typeof errors[name]?.message === 'string' && (
+                    <p className={`text-red-500 text-[0.75rem]`}>{errors[name]?.message}</p>
+                )}</p>
             </div>
 
         </div>
